1300323ed9088609cbb5b2b0b1e30a9c3b4467c2
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2023-10-12 10:58:59 +0300

    Отформатировал код в  active_passcards_view.py

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -1,10 +1,14 @@
 from datacenter.models import Passcard, Visit
+
+
 from django.shortcuts import render
 
 
 def active_passcards_view(request):
     active_passcards = Passcard.objects.filter(is_active = True)
+    
     context = {
         'active_passcards': active_passcards,
     }
+    
     return render(request, 'active_passcards.html', context)
